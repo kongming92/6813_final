664ae6c0f3655b8dcd21a6938f694bd638ce867a
khl1392 <khl1392@gmail.com>
2012-04-14 18:53:42 -0400

    some working links

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 /* Fat Charles 6.813 JS File
 */
-$(document).on('pageinit', '#homePage',function() {//can add a selector
+$(document).on('pageinit', '#homePage', function() {//can add a selector
 	$('div [data-role="controlgroup"]').addClass('ui-shadow');
 });
+
+$(document).on('pageinit', '#namePage', function() {
+	setTimeout("$('input:first').focus()", 100);
+	$('input[type="submit"]').click(function() {
+		alert($('input[type=""]'));
+	});
+});
+
