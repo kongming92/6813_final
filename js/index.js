99828cb288481ee6e6c77949bc5b00b9460a8e2b
Charles Liu <charlesliu@cliumbp.local>
2012-05-08 03:09:13 -0400

    stuff

--- js/index.js
+++ js/index.js
@@ -31,15 +31,35 @@ $(document).on('pageinit', '#namePage', function() {
 	});
 	$("#searchButton").click(function() {
 		$("#searchResults").empty();
+		var opts = {
+			  lines: 13, // The number of lines to draw
+			  length: 7, // The length of each line
+			  width: 4, // The line thickness
+			  radius: 10, // The radius of the inner circle
+			  rotate: 0, // The rotation offset
+			  color: '#000', // #rgb or #rrggbb
+			  speed: 1, // Rounds per second
+			  trail: 60, // Afterglow percentage
+			  shadow: false, // Whether to render a shadow
+			  hwaccel: false, // Whether to use hardware acceleration
+			  className: 'spinner', // The CSS class to assign to the spinner
+			  zIndex: 2e9, // The z-index (defaults to 2000000000)
+			  top: 'auto', // Top position relative to parent in px
+			  left: 'auto' // Left position relative to parent in px
+		};
+		var target = document.getElementById('searchResults');
+		var spinner = new Spinner(opts).spin(target);
 		var searchString = $("#drinkName").val();
 		var searchURL = "php/searchByName.php";
 		if ($.trim(searchString) == "") {
 			return;
 		}
+				
 		$.post(
 			searchURL,
 			{ param : searchString },
 			function(data) {
+				spinner.stop();
 				if (data.length == 0) {
 					$("#searchResults").append("<p>No drinks matched your search input</p>");
 				} else {
@@ -55,6 +75,7 @@ $(document).on('pageinit', '#namePage', function() {
 			},
 			"json"
 		);
+		
 	});
 });
 
--- php/searchByName.php
+++ php/searchByName.php
@@ -11,7 +11,7 @@ mysql_select_db("fat_charles_db");
 
 
 $result = array();
-$results = mysql_query("select instructions.id, instructions.drink_name, ratings.rating from instructions, ratings where instructions.id = ratings.id and instructions.drink_name like '%$search%' order by ratings.rating desc;");
+$results = mysql_query("select instructions.id, instructions.drink_name, ratings.rating from instructions, ratings where instructions.id = ratings.id and instructions.drink_name like '%$search%' order by ratings.rating desc LIMIT 100");
 while(list($id,$drinkname,$rating) = mysql_fetch_array($results)){
 	$result[] = json_encode(array("id" => $id, "name" => $drinkname, "rating" => $rating));
 }
